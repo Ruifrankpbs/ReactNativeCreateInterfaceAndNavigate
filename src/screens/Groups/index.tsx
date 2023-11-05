9418149b3644dc36d0ff96e6e60dbe59f2d2994f
Rui Frank <ruifrankpbs@gmail.com>
2023-11-05 15:19:08 -0300

    CRIANDO O COMPONENTE HIGHLIGHT QUE É O TITULO E SUBTITULO QUE EXIBIREMOS NAS TELAS

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -2,13 +2,18 @@
 
 
 import { Header } from "@components/Header";
+import { Highlight } from "@components/Highlight";
 import { Container } from "./styles";
 
 export function Groups() {
 
   return (
     <Container>
-      <Header mostrarBackButton />
+      <Header />
+      <Highlight
+        title="Turmas"
+        subtitle="jogue com sua turma"
+      />
     </Container>
   );
 
